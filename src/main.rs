dd232f1a519097d0bd0c69987a86c69fbf3d733b
Miel Peeters <peetersmiel@gmail.com>
2023-05-16 10:04:48 +0200

    Implement sorting of a deck
    Also created a way of making a new full deck, or a new deck from a vector of cards.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 #![allow(dead_code)]
 use rand::seq::SliceRandom;
 use std::fmt;
+use std::cmp::Ordering;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Eq, Ord, PartialEq, PartialOrd)]
 enum Colour {
-    Hearts,
     Spades,
-    Diamonds,
     Clubs,
+    Diamonds,
+    Hearts,
 }
 
+#[derive(Eq, Copy, Clone)]
 struct Card {
     colour: Colour,
     number: u8,
@@ -17,25 +19,101 @@ struct Card {
 
 struct Deck {
     cards: Vec<Card>,
+    /// keeps the amount of cards of each colour type
+    amounts: Vec<usize>,
+}
+
+struct Game {
+    /// tricks keeps track of the played tricks
+    /// one trick is a deck of 4 cards
+    tricks: Vec<Deck>,
 }
 
 impl Deck {
-    fn pull_cards(&mut self, amount: usize) -> Deck {
-        let pulled = self.cards.drain(..amount).collect();
-        Deck { cards: pulled }
+    pub fn new_full() -> Self {
+        let mut cards: Vec<Card> = Vec::new();
+
+        let colours = [
+            Colour::Hearts,
+            Colour::Clubs,
+            Colour::Diamonds,
+            Colour::Spades,
+        ];
+
+        for clr in colours {
+            for nmb in 1..14 {
+                cards.push(Card {
+                    colour: clr,
+                    number: nmb,
+                });
+            }
+        }
+
+        let amounts: Vec<usize> = vec![13, 13, 13, 13];
+
+        Deck { cards, amounts }
     }
 
-    fn size(&self) -> usize {
+    fn get_amounts(cards: &Vec<Card>) -> Vec<usize> {
+        let mut amounts: Vec<usize> = Vec::new();
+        amounts.push(0);
+        amounts.push(0);
+        amounts.push(0);
+        amounts.push(0);
+        
+        // count the amounts
+        for card in cards {
+            match card.colour {
+                Colour::Spades => amounts[0] += 1,
+                Colour::Clubs => amounts[1] += 1,
+                Colour::Diamonds => amounts[2] += 1,
+                Colour::Hearts => amounts[3] += 1,
+            }
+        }
+
+        amounts
+    }
+
+    pub fn new_from(cards: Vec<Card>) -> Deck {
+        let amounts = Self::get_amounts(&cards);
+        Deck { cards, amounts}
+    }
+
+    pub fn pull_cards(&mut self, amount: usize) -> Deck {
+        let pulled = self.cards.drain(..amount).collect();
+        let amounts = Self::get_amounts(&pulled);
+        Deck { cards: pulled , amounts}
+    }
+
+    pub fn size(&self) -> usize {
         self.cards.len()
     }
 
-    fn shuffle(&mut self) {
+    pub fn shuffle(&mut self) {
         self.cards.shuffle(&mut rand::thread_rng());
     }
 
     /// sort the cards by colour first, then by ascending number
-    fn sort(&mut self) {
-        // TODO
+    pub fn sort(&mut self) {
+        self.cards.sort();
+    }
+
+    pub fn get_deck_of_colour(&self, colour: Colour) -> Deck {
+        let start = match colour {
+            Colour::Spades => 0,
+            Colour::Clubs => self.amounts[0],
+            Colour::Diamonds => self.amounts[0] + self.amounts[1],
+            Colour::Hearts => self.size() - self.amounts[3],
+        };
+
+        let cards: Vec<Card> = match colour {
+            Colour::Spades => self.cards[start..start+self.amounts[0]].to_vec(),
+            Colour::Clubs => self.cards[start..start+self.amounts[1]].to_vec(),
+            Colour::Diamonds => self.cards[start..start+self.amounts[2]].to_vec(),
+            Colour::Hearts => self.cards[start..start+self.amounts[3]].to_vec(),
+        };
+
+        Self::new_from(cards)
     }
 }
 
@@ -60,6 +138,10 @@ impl fmt::Display for Card {
         if nb == "1" {
             nb = String::from("A");
         }
+        // Ten
+        else if nb == "10" {
+            nb = String::from("T");
+        }
         // Jack
         else if nb == "11" {
             nb = String::from("J");
@@ -74,6 +156,34 @@ impl fmt::Display for Card {
         }
 
         write!(f, "{}{}", nb, self.colour)
+    }
+}
+
+impl Ord for Card {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.colour != other.colour {
+            self.colour.cmp(&other.colour)
+        } else {
+            if self.number == 1 {
+                Ordering::Greater
+            } else if other.number == 1 {
+                Ordering::Less
+            } else {
+                self.number.cmp(&other.number)
+            }
+        }
+    }
+}
+
+impl PartialOrd for Card {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Card {
+    fn eq(&self, other: &Self) -> bool {
+        self.colour == other.colour && self.number == other.number
     }
 }
 
@@ -95,42 +205,24 @@ impl fmt::Display for Deck {
     }
 }
 
-fn full_deck() -> Deck {
-    let mut cards: Vec<Card> = Vec::new();
-
-    let colours = [
-        Colour::Hearts,
-        Colour::Clubs,
-        Colour::Diamonds,
-        Colour::Spades,
-    ];
-
-    for clr in colours {
-        for nmb in 1..14 {
-            cards.push(Card {
-                colour: clr,
-                number: nmb,
-            });
-        }
-    }
-
-    Deck { cards }
-}
-
 fn main() {
-    let mut deck = full_deck();
+    let mut deck = Deck::new_full();
 
     deck.shuffle();
 
-    let player_one = deck.pull_cards(13);
+    let mut player_one = deck.pull_cards(13);
+    player_one.sort();
     println!("Player one's deck: {}\n", player_one);
 
-    let player_two = deck.pull_cards(13);
+    let mut player_two = deck.pull_cards(13);
+    player_two.sort();
     println!("Player two's deck: {}\n", player_two);
 
-    let player_three = deck.pull_cards(13);
+    let mut player_three = deck.pull_cards(13);
+    player_three.sort();
     println!("Player three's deck: {}\n", player_three);
 
-    let player_four = deck.pull_cards(13);
+    let mut player_four = deck.pull_cards(13);
+    player_four.sort();
     println!("Player four's deck: {}\n", player_four);
 }
